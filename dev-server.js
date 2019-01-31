260204c33ff3eae484d0db1990f5594bd61092f8
Patrick Mueller <pmuellr@nodesource.com>
2019-01-31 01:51:55 -0500

    add request logging for dev server

--- dev-server.js
+++ dev-server.js
@@ -7,6 +7,7 @@ const pkg = require('./package.json')
 process.title = pkg.name
 
 const PORT = process.env.PORT || '3001'
+const PORT_API = process.env.PORT_API || '3000'
 
 // if invoked as main, call start()
 if (require.main === module) setImmediate(start)
@@ -21,7 +22,7 @@ let Server
 function redirectAPI (req, res, next) {
   const url = req.originalUrl
 
-  res.redirect(`http://localhost:3000${url}`)
+  res.redirect(`http://localhost:${PORT_API}${url}`)
 }
 
 // start the server
@@ -33,6 +34,9 @@ async function start (options) {
   const app = express()
   app.set('env', 'production')
   app.set('json spaces', 2)
+
+  // request logger
+  app.use(requestLogger)
 
   // redirect api calls
   app.use('/api/*', redirectAPI)
@@ -70,12 +74,41 @@ async function start (options) {
 
   // start the server
   Server = app.listen(PORT, () => {
-    console.log(`http server is listening at port ${PORT}`)
+    console.log(`dev http server is listening at port ${PORT}`)
+    console.log(`expecting api server listening at port ${PORT_API}`)
   })
 }
 
 // set up exit handlers
 let initializedErrorHandlers = false
+
+function requestLogger (req, res, next) {
+  res.once('finish', finished)
+
+  res.locals.requestStartTime = Date.now()
+
+  next()
+
+  // function called when response is finally sent (to the OS)
+  function finished () {
+    const elapsedMS = `${Date.now() - res.locals.requestStartTime}`
+
+    let date = new Date()
+    date = new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000)
+    const time = date.toISOString().substr(11, 12)
+
+    const logLine = `
+      ${time}
+      http
+      ${res.statusCode}
+      ${elapsedMS.padStart(5, '😀')}ms
+      ${req.method}
+      ${req.originalUrl}
+    `.replace(/\s+/g, ' ').trim().replace(/😀/g, ' ')
+
+    console.log(logLine)
+  }
+}
 
 function setupErrorHandlers () {
   if (initializedErrorHandlers) return
